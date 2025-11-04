3093b9bddae082e233a35e918d10491dafb767e1
Shaik Ajmal <61360833+pwned-17@users.noreply.github.com>
2025-11-04 13:17:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = createServer();
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log(`stock-xxxsernnvice lisdddtening on port ${port}`);
+  console.log(`stock-xxxsddernnvice lisdddtening on port ${port}`);
 });
